3b8f5dd8869958e2184673aa7b94f049363afabb
Diego Bruno <diego@exein.io>
2025-10-15 17:27:13 +0200

    - fixed generation script

--- utils/generate-share.sh
+++ utils/generate-share.sh
@@ -3,7 +3,7 @@
 YOCTO_BUILD_DIR=~/workdir/poky/build
 
 mkdir -p tmp/yocto-qemu-share
-cp $YOCTO_BUILD_DIR/tmp/deploy/images/qemux86-64/core-image-minimal-qemux86-64.* tmp/yocto-qemu-share/
+cp $YOCTO_BUILD_DIR/tmp/deploy/images/qemux86-64/yocto-qemux86-64.* tmp/yocto-qemu-share/
 cp $YOCTO_BUILD_DIR/tmp/deploy/images/qemux86-64/bzImage-qemux86-64.bin tmp/yocto-qemu-share/
 cd tmp/yocto-qemu-share
 tar czf ../../yocto-qemu-share.tar.gz *
