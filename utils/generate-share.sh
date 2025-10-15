3a273cce7911d5f7c67f0286b2fdeb881378f3a7
Diego Bruno <diego@exein.io>
2025-10-15 16:03:56 +0200

    first commit

---
+++ utils/generate-share.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+YOCTO_BUILD_DIR=~/workdir/poky/build
+
+mkdir -p tmp/yocto-qemu-share
+cp $YOCTO_BUILD_DIR/tmp/deploy/images/qemux86-64/core-image-minimal-qemux86-64.* tmp/yocto-qemu-share/
+cp $YOCTO_BUILD_DIR/tmp/deploy/images/qemux86-64/bzImage-qemux86-64.bin tmp/yocto-qemu-share/
+cd tmp/yocto-qemu-share
+tar czf ../../yocto-qemu-share.tar.gz *
+cd ../..
+
+mkdir -p tmp/yocto-vb-share
+cp $YOCTO_BUILD_DIR/tmp/deploy/images/qemux86-64/yocto-vbx86-64.wic.vdi tmp/yocto-vb-share/
+cd tmp/yocto-vb-share
+tar czf ../../yocto-vb-share.tar.gz *
+
